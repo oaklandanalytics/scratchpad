983067a09a48fc19e862000924354b73059e283a
Fletcher Foti <fscottfoti@gmail.com>
2016-09-08 11:51:31 -0700

    same analysis on run 5080

---
+++ max_dua_policy_zones/go5080.py
@@ -0,0 +1,26 @@
+import folium
+import pandas as pd
+import json
+
+geo = '../data/policy_zones.geojson'
+
+all_ids = pd.Series([
+    feature["properties"]["zoningmodc"]
+    for feature in json.load(open(geo))["features"]
+])
+
+df = pd.read_csv("max_density_per_policyzone_5080.csv").set_index("zoningmodcat")
+
+# folium doesn't like missing ids
+df = df.reindex(all_ids).reset_index()
+df.rename(columns={'index': 'zoningmodcat'}, inplace=True)
+df["max_built_dua"] = df.max_built_dua.fillna(0)
+print df.max_built_dua.describe()
+
+map = folium.Map(location=[37.8, -122.4], zoom_start=11, tiles='cartodbpositron')
+map.choropleth(geo_path=geo, data=df,
+             columns=['zoningmodcat', 'max_built_dua'],
+             key_on='feature.properties.zoningmodc',
+             fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2,
+             legend_name='Max Dua by Policy Zone')
+map.save('max_dua_policy_zones_5080.html')
